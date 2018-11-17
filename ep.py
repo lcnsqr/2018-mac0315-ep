d6bb23bb3c4ef9e69ee7af7b3382008ce27b6415
Luciano Siqueira <lcnsqr@gmail.com>
2018-11-17 18:46:03 -0200

    Inclusão de mais comentários

--- ep.py
+++ ep.py
@@ -1,20 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
-from numpy import zeros, array, linspace
+# Construção do problema apresentado no enunciado do EP.
+# A discretização é feita a partir de um valor inteiro informado 
+# na linha de comando, chamado fator de discretização. O fator de
+# discretização deve ser maior ou igual a 2.
+# Por exemplo, se o comando for invocado na forma
+#   ./ep.py 4
+# então o tempo será discretizado em 4 partes. Assim, tomando d = 4, as funções 
+# aceleração a(t), velocidade v(t) e distância x(t) estarão discretizadas
+# em três vetores, "a", "v" e "x", cada um com 5 (d+1) componentes. 
+# Seja "T" o intervalo de tempo analisado, então a relação entre a discretização 
+# e o problema contínuo será:
+#   a_i = a(i*T/d)
+#   v_i = v(i*T/d)
+#   x_i = x(i*T/d)
+# onde o "i" é um índice de 0 a d.
 
+import sys
+import numpy as np
+
+# O algoritmo Simplex está implementado no arquivo simplex.py
 from simplex import otimo
 
-from octave import formatOctave
-
+# O fator de discretização deve ser informado como parâmetro para o programa.
 if len(sys.argv) < 2:
     exit("Informe o fator de discretização")
 
-# Fator de discretização (número de intervalos no tempo)
+# d: fator de discretização (número de intervalos no tempo).
+# É a partir deste parâmetro que todas as demais dimensões são computadas.
 d = int(sys.argv[1])
-if d < 1:
-    exit("O fator de discretização deve ser maior ou igual a um")
+if d < 2:
+    exit("O fator de discretização deve ser maior ou igual a 2")
 
 # Intervalo de tempo
 T = 10.0
@@ -26,18 +43,18 @@ X = 1.0
 p = T/d
 
 # Lado esquerdo das restrições (matriz A)
-A = zeros((4*d+4, 5*d+2))
+A = np.zeros((4*d+4, 5*d+2))
 
-# Restrições originais
+# Restrições originais do problema
 for i in range(d):
-    # Primeira linha
+    # Primeira linha do i-ésimo par de restrições
     # a
     A[2*i,2*i] = p
     A[2*i,2*i+1] = -p
     # v
     A[2*i,2*d+i] = 1
     A[2*i,2*d+i+1] = -1
-    # Segunda linha
+    # Segunda linha do i-ésimo par de restrições
     # a
     A[2*i+1,2*i] = -p**2
     A[2*i+1,2*i+1] = p**2
@@ -66,14 +83,15 @@ A[4*d+2,3*d+1] = 1
 A[4*d+3,4*d+1] = 1
 
 # Lado direito das restrições (vetor b)
-b = zeros(4*d+4)
+b = np.zeros(4*d+4)
+# Apenas a componente correspondente a x_d é igual a 1 (as demais são zero).
 b[-1] = 1
 
 # Comparação das restrições
 # 0: Igual a b
 # 1: Menor que b
 # -1: Maior que b
-comp = zeros(4*d+4)
+comp = np.zeros(4*d+4)
 comp[2*d:4*d] = -1
 
 # Sentido do PL
@@ -82,12 +100,10 @@ comp[2*d:4*d] = -1
 s = 1;
 
 # Coeficientes da função objetivo
-c = zeros(5*d+2)
+c = np.zeros(5*d+2)
 c[4*d+2:5*d+2] = 1
 
-# Produzir saída para testar com Octave
-#formatOctave(d, A, b, comp, s, c)
-
+# Encontrar a variável y que minimiza o PL e o valor ótimo f
 y,f,err = otimo(s,c,A,b,comp)
 if err != 0:
     # Não foi possível encontrar solução
@@ -105,15 +121,24 @@ if err != 0:
         print("Problema auxiliar ilimitado")
     sys.exit(err)
 
-# Aceleração
-a = zeros(d+1)
+# Compor os vetores de interesse a partir da solução encontrada
+# Aceleração: Vetor "a"
+a = np.zeros(d+1)
 for i in range(d):
+    # Reunir parte positiva e negativa da variável irrestrita
     a[i] = y[2*i] - y[2*i+1] 
-# Velocidade
+# Velocidade: Vetor "v"
 v = y[2*d:3*d+1]
-# Distância
+# Distância: Vetor "x"
 x = y[3*d+1:4*d+2]
 
-print("Tempo\tAceleração\tVelocidade\tDistância")
+# Exibir o resultado em quatro colunas: t, a(t), v(t), x(t)
+print("t\ta(t)\tv(t)\tx(t)")
 for i in range(d+1):
     print('{:.6f}\t{:.6f}\t{:.6f}\t{:.6f}'.format(i*p, a[i], v[i], x[i]))
+# Valor ótimo da função objetivo.
+# Soma dos valores absolutos do vetor "a"
+f = 0
+for i in range(d):
+    f = f + abs(a[i])
+print >> sys.stderr, 'Valor ótimo: {:.6f}'.format(f)
--- simplex.py
+++ simplex.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
-import numpy
+# Implementação do algoritmo Tableau Simplex em duas fases.
+# Variáveis de folga são incluídas automaticamente.
 
-from numpy import array, eye, concatenate, zeros, empty, matmul, append, flatnonzero, ones
+# O programa é composto por cinco funções:
+# otimo: Monta o problema para as duas fases do Tableau Simplex, invoca o algoritmo e retorna o resultado.
+# tabRodar: Itera no tableau enquanto não encontrar um critério de parada.
+# tabEntra: Identifica uma coluna que melhora a função objetivo usando a Regra de Bland.
+# tabSai: Identifica a coluna que sai usando o critério da razão.
+# tabTrocar: Efetua a troca de base no tableau.
+
+import sys
+import numpy as np
 
 # O arredondamento dos resultados pode não produzir zero, mas um valor ínfimo que deve ser tratado 
 # como zero, pois pode quebrar o algoritmo (seria zero não fosse o erro de arredondamento da máquina).
-INFIMO = 1e-14
+INFIMO = 1e-12
 
 # Trocar coluna básica por coluna não-básica no Tableau.
 # Retorna o Tableau atualizado.
@@ -20,7 +28,7 @@ def tabTrocar(T, l, c):
     # Quantidade de linhas no Tableau
     m = T.shape[0]
     # Matriz de conversão
-    Q = eye(m)
+    Q = np.eye(m)
     for i in range(m):
         if i == l:
             # Inverso do pivô, resultará em 1.
@@ -31,7 +39,41 @@ def tabTrocar(T, l, c):
             Q[i,l] = -T[i,c]/T[l,c]
 
     # Retornar Tableau atualizado pela matriz de conversão Q
-    return matmul(Q, T)
+    return np.matmul(Q, T)
+
+# Identificar linha no Tableau correspondente à coluna que sai base.
+# Retorna o índice da linha no Tableau ou -1 se nenhuma linha atende ao critério.
+# Parâmetros:
+# T: Tableau Simplex
+# c: Índice da coluna no Tableau correspondente à direção viável (coluna que entra)
+def tabSai(T, c):
+    # Quantidade de linhas no Tableau.
+    m = T.shape[0]
+    # Avaliar apenas para os componentes positivos da direção viável.
+    # Conjunto L armazena os índices das linhas onde o valor da direção viável é positivo.
+    L = np.empty(0, dtype=int)
+    for i in range(1,m):
+        # Incluir se valor positivo
+        if T[i,c] > INFIMO:
+            L = np.append(L, i)
+    # Determinar coluna que sai pelo critério da razão, usando 
+    # como denominador os valores positivos da direção viável.
+    if len(L) > 0:
+        # Existe pelo menos uma componente positiva na direção.
+        # É o índice escolhido se não houverem outros.
+        l = L[0]
+        # Comparar com demais componentes positivas (se houverem).
+        if len(L) > 1:
+            for i in L[1:]:
+                # Trocar o índice escolhido se satisfizer o critério da razão.
+                if T[i,0]/T[i,c] < T[l,0]/T[l,c]:
+                    l = i
+        return l 
+    else:
+        # Não existe componente positiva.
+        # Direção viável melhora mas é ilimitada
+        print(T[:,c])
+        return -1
 
 # Identificar coluna no Tableau que entra na base.
 # Retorna o índice da coluna no Tableau ou -1 se nenhuma coluna atende ao critério.
@@ -55,40 +97,6 @@ def tabEntra(T, N, s):
     # Retorna -1 se nenhum custo reduzido melhora a função objetivo
     return -1
 
-# Identificar linha no Tableau correspondente à coluna que sai base.
-# Retorna o índice da linha no Tableau ou -1 se nenhuma linha atende ao critério.
-# Parâmetros:
-# T: Tableau Simplex
-# c: Índice da coluna no Tableau correspondente à direção viável (coluna que entra)
-def tabSai(T, c):
-    # Quantidade de linhas no Tableau.
-    m = T.shape[0]
-    # Avaliar apenas para os componentes positivos da direção viável.
-    # Conjunto L armazena os índices das linhas onde o valor da direção viável é positivo.
-    L = empty(0, dtype=int)
-    for i in range(1,m):
-        # Incluir valor positivo
-        if T[i,c] > INFIMO:
-            L = append(L, i)
-    # Determinar coluna que sai pelo critério da razão, usando 
-    # como denominador os valores positivos da direção viável.
-    if len(L) > 0:
-        # Existe pelo menos uma componente positiva na direção.
-        # É o índice escolhido se não houverem outros.
-        l = L[0]
-        # Comparar com demais componentes positivas (se houverem).
-        if len(L) > 1:
-            for i in L[1:]:
-                # Trocar o índice escolhido se satisfizer o critério da razão.
-                if T[i,0]/T[i,c] < T[l,0]/T[l,c]:
-                    l = i
-        return l 
-    else:
-        # Não existe componente positiva.
-        # Direção viável melhora mas é ilimitada
-        print(T[:,c])
-        return -1
-
 # Iterar no Tableau até um critério de parada.
 # Retorna o Tableau final, lista de índices básicos e não básicos e o erro (0: Sem erro, 2: Problema ilimitado)
 # Parâmetros:
@@ -99,9 +107,11 @@ def tabSai(T, c):
 def tabRodar(T, B, N, s):
     # Identificar coluna no Tableau que entra na base
     c = tabEntra(T, N, s)
+    # Continuar enquanto existirem variáveis não-básicas que melhoram a função objetivo.
     while c > 0:
         # Converter para índice da nova variável básica
         k = c - 1
+        # Identificar a variável que sai da base.
         l = tabSai(T, c)
         if l > 0:
             # Trocar base
@@ -118,6 +128,15 @@ def tabRodar(T, B, N, s):
     # Solução ótima encontrada
     return T, B, N, 0
 
+# Encontrar o valor ótimo a partir de um PL na forma geral.
+# Prepara o PL e executa as duas fases do Tableau Simplex.
+# Retorna o vetor ótimo, o valor da função objetivo e o erro (se houver).
+# Parâmetros:
+# s: Sentido do PL. 1 é Minimizar, -1 é Maximizar
+# c: Coeficientes da função objetivo
+# A: Lado esquerdo das restrições (matriz A)
+# b: Lado direito das restrições (vetor b)
+# comp: Vetor com as comparações das restrições. 0 indica igual a b, 1: Menor que b, -1: Maior que b
 def otimo(s,c,A,b,comp):
     # Quantidade de linhas na matriz de restrições
     m = A.shape[0]
@@ -136,25 +155,28 @@ def otimo(s,c,A,b,comp):
     slack = 0
     for i in range(len(comp)):
         if comp[i] != 0:
-            A = concatenate((A, comp[i] * eye(m)[i,:].reshape(-1,1)), axis=1)
-            c = append(c, 0)
+            # O sinal da variável de folga depende da comparação na restrição correspondente
+            A = np.concatenate((A, comp[i] * np.eye(m)[i,:].reshape(-1,1)), axis=1)
+            # Incluir um coeficiente nulo para a variável de folga na função objetivo
+            c = np.append(c, 0)
+            # Contador das variáveis de folga
             slack = slack + 1
     # Construir Tableau do problema auxiliar
-    T_aux = empty((1+m, 1+n+slack+m))
+    T_aux = np.empty((1+m, 1+n+slack+m))
     # Valor da função objetivo:
-    T_aux[0,0] = -matmul(ones((1, m)), b)[0]
+    T_aux[0,0] = -np.matmul(np.ones((1, m)), b)[0]
     # Variáveis ativas da solução:
     T_aux[1:,0] = b
     # Colunas correspondentes às direções viáveis e não viáveis:
-    T_aux[1:,1:] = concatenate((A, eye(m)), axis=1)
+    T_aux[1:,1:] = np.concatenate((A, np.eye(m)), axis=1)
     # Lista dos índices das variáveis não-básicas
     N_aux = range(n+slack)
     # Lista dos índices das variáveis básicas
     B_aux = range(n+slack, n+slack+m)
     # Custos reduzidos do problema auxiliar:
-    c_aux = zeros(n+slack+m)
+    c_aux = np.zeros(n+slack+m)
     c_aux[n+slack:] = 1
-    T_aux[0,1:] = c_aux - matmul(c_aux[B_aux], T_aux[1:,1:])
+    T_aux[0,1:] = c_aux - np.matmul(c_aux[B_aux], T_aux[1:,1:])
     # Encontrar uma solução viável:
     T_aux, B_aux, N_aux, err = tabRodar(T_aux, B_aux, N_aux, 1)
     if err != 0:
@@ -168,18 +190,19 @@ def otimo(s,c,A,b,comp):
     N_aux.sort()
     N = N_aux[:-m]
     # Valor da função objetivo:
-    T[0,0] = -matmul(c[B], b)
+    T[0,0] = -np.matmul(c[B], b)
     # Custos reduzidos:
-    T[0,1:] = c - matmul(c[B], T[1:,1:])
+    T[0,1:] = c - np.matmul(c[B], T[1:,1:])
     # Encontrar solução ótima do problema original:
     T, B, N, err = tabRodar(T, B, N, s)
     if err != 0:
         # Não foi possível encontrar solução do problema original
         return T_aux[1:,0], T_aux[0,0], err
     # Compor solução a partir das variáveis ativas
-    x = zeros(n)
+    x = np.zeros(n)
     for i in range(m):
         # Ignorar variáveis de folga na base
         if B[i] < n:
             x[B[i]] = T[1+i,0]
+    # Retornar o ótimo
     return x, -T[0,0], err
